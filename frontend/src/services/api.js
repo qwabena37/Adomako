a0e2e49b4f307ea675182aa7a92054e7b335eaf4
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-08-04 22:32:03 +0000

    modify render api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-  "https://adomako-3.onrender.com/api/",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
