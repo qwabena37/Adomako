0976b2a58e968d98e83a99bf284f26749fa7b70e
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-08-04 20:40:17 +0000

    modify rest_frame in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,13 +104,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
+
+        'rest_framework.authentication.SessionAuthentication',
+
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+
     ],
 
     'DEFAULT_FILTER_BACKENDS': [
+
         'django_filters.rest_framework.DjangoFilterBackend',
+
         'rest_framework.filters.SearchFilter',
+
         'rest_framework.filters.OrderingFilter',
+
     ],
 }
 
