f747731d253b11bc8fddb41590d75cf37d2fef7f
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-08-05 09:32:32 +0000

    fix missing serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Product, Category, Inquiry
 
+
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
@@ -15,3 +16,17 @@ class ProductSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(obj.image.url)
 
         return None
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class InquirySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inquiry
+        fields = "__all__"
