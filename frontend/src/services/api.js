d04c9e3af07aa01a24230428432258cf2ad5a069
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-08-05 18:10:08 +0000

    modify ProductsAdmin.jsx

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,23 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
 
+
+api.interceptors.request.use(
+    (config) => {
+
+        const token = localStorage.getItem("access");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
 export default api;
