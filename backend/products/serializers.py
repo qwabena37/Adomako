9099031128f1be5472778d4973afb4985827efc7
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-08-06 17:15:05 +0000

    Fix Cloudinary image URLs

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -13,26 +13,16 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-
-        request = self.context.get("request")
-
-        if instance.image and request:
-            data["image"] = request.build_absolute_uri(
-                instance.image.url
-            )
-
-        return data
-
 
 class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
         fields = "__all__"
 
 
 class InquirySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Inquiry
         fields = "__all__"
